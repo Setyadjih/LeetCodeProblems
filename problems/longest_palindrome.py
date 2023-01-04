201923f42e540621a54b7dfcc1c7a0c834eb6476
Hugo Setyadji <setyadjih@gmail.com>
2023-01-04 21:06:28 +0700

    feat: problems

--- problems/longest_palindrome.py
+++ problems/longest_palindrome.py
@@ -1,22 +1,33 @@
-from collections import Counter
-
-
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
+        if len(s) == 1:
+            return s
+        if s == s[::-1]:
+            return s
+        longest = ""
+
+        # find the longest palindrome from each letter as the center
+        # do again for even sized palindromes
         for i in range(len(s)):
-            odd = self.expandCenter(s, i, i)
-            even = self.expandCenter(s, i, i+1)
-            res = max(res, odd, even, key=len)
-        return res
+            start = i
+            end = i
+            while start >= 0 and end < len(s) and s[start] == s[end]:
+                if (end - start + 1) > len(longest):
+                    longest = s[start:end+1]
 
-    def expandCenter(self, s, left, right):
-        while left > 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
+                start -= 1
+                end += 1
 
-        return s[left+1:right]
+            start, end = i, i + 1
+            while start >= 0 and end < len(s) and s[start] == s[end]:
+                # get length of palindrome
+                if (end - start + 1) > len(longest):
+                    longest = s[start:end + 1]
 
+                start -= 1
+                end += 1
+
+        return longest
 
 if __name__ == '__main__':
-    print(Solution().longestPalindrome("cbbd"))
+    print(Solution().longestPalindrome("babad"))
